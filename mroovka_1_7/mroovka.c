3bf926a28b3d7641f5b1f6afe2ba8a04128870d1
MlodyGawciu <kubagawart@op.pl>
2018-03-20 11:15:55 +0100

    nic nie ma w 1.7 prawie, duperele

---
+++ mroovka_1_7/mroovka.c
@@ -0,0 +1,93 @@
+#include<stdlib.h>
+#include"mroovka.h"
+#include"macierz.h"
+
+void ruch ( mrowka_t mrowka, int kolor, macierz_t macierz, int ktoramrowka )
+{
+	switch ( mrowka->kierunek[ktoramrowka] )
+	{
+		case PLN:
+			if( kolor == ktoramrowka )
+			{
+				mrowka->kierunek[ktoramrowka] = ZACH;
+				mrowka->x[ktoramrowka] -=1;
+			}
+			else if( 1 )
+			{
+				mrowka->kierunek[ktoramrowka] = WSCH;
+				mrowka->x[ktoramrowka] +=1;
+			}
+			przejscie( mrowka, macierz, ktoramrowka );
+		break;
+
+		case WSCH:
+			if( kolor == ktoramrowka )
+			{
+				mrowka->kierunek[ktoramrowka] = PLN;
+	                        mrowka->y[ktoramrowka] -=1;
+			}
+			else if( 1 )
+			{
+				mrowka->kierunek[ktoramrowka] = PLD;
+				mrowka->y[ktoramrowka] +=1;
+				}
+			przejscie( mrowka, macierz, ktoramrowka );
+		break;
+
+		case PLD:
+			if( kolor == ktoramrowka )
+			{
+				mrowka->kierunek[ktoramrowka] = WSCH;
+				mrowka->x[ktoramrowka] +=1;
+
+			}
+			else if( 1 )
+			{
+				mrowka->kierunek[ktoramrowka] = ZACH;
+				mrowka->x[ktoramrowka] -=1;
+			}
+			przejscie( mrowka, macierz, ktoramrowka );
+		break;
+
+		case ZACH:
+			if( kolor == ktoramrowka )
+			{
+				mrowka->kierunek[ktoramrowka] = PLD;
+				mrowka->y[ktoramrowka] +=1;
+			}
+			else if( 1 )
+			{
+				mrowka->kierunek[ktoramrowka] = PLN;
+				mrowka->y[ktoramrowka] -=1;
+			}
+			przejscie( mrowka, macierz, ktoramrowka );
+		break;
+	}	
+}
+
+void przejscie( mrowka_t mrowka, macierz_t macierz, int ktora )
+{
+	if ( mrowka->x[ktora] < 0 )
+		mrowka->x[ktora] = macierz->kolumny - 1;
+	else if ( mrowka->x[ktora] >= macierz->kolumny )
+		mrowka->x[ktora] = 0;
+
+	if ( mrowka->y[ktora] < 0 )
+		mrowka->y[ktora] = macierz->wiersze - 1;
+	else if ( mrowka->y[ktora] >= macierz->wiersze )
+		mrowka->y[ktora] = 0;
+}
+
+void rozdzielenie ( mrowka_t mrowka, int ilosc, int ktora) 
+{
+	int i;
+	for ( i = ktora + 1; i< ilosc+1; i++ )
+	{
+		if ( mrowka->y[ktora] == mrowka->y[i] && mrowka->x[ktora] == mrowka->x[i] )
+		{
+			mrowka->kierunek[ktora] = PLN;
+			mrowka->kierunek[i] = PLD;
+		}
+	}
+
+}
---
+++ mroovka_1_7/obsluga.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include"obsluga.h"
+#include"mroovka.h"
+#include"macierz.h"
+
+void wypisz( macierz_t macierz, mrowka_t mrowka, int ilosc )
+{
+	int i, j, k, l, wyjdz;
+	for ( i = 0; i < macierz->wiersze; i++ )
+        {
+       		for ( j = 0; j < macierz->kolumny; j++ )
+        	{
+				wyjdz = 0;
+		
+				for ( l = 1; l < ilosc+1; l++ )
+				{
+					if ( j == mrowka->x[l] && i == mrowka->y[l] )
+					{
+						printf("\x1B[36m m");
+						wyjdz = 1;
+						break;
+					}
+				}
+
+				if ( wyjdz == 1 )
+					continue;
+        			else if ( macierz->wart[i][j] == 1 )
+        				printf("\x1B[31m 1");
+				else if ( macierz->wart[i][j] == 2 )
+                                        printf("\x1B[32m 2");
+				else if ( macierz->wart[i][j] == 3 )
+                                        printf("\x1B[33m 3");
+				else if ( macierz->wart[i][j] == 4 )
+                                        printf("\x1B[34m 4");
+				else if ( macierz->wart[i][j] == 5 )
+                                        printf("\x1B[35m 5");
+  		      		else
+        				printf("\x1B[30m 0\x1B[37m");
+
+        	}	
+        	printf("\n");
+      	}
+}
